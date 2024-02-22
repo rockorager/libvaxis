845a3a67861d8bb709b98727915e811bec2a2b4f
00JCIV00 <jake.crawfordiv@gmail.com>
2024-02-22 16:54:07 -0500

    Added  build step to .

---
+++ docs/data-modules.js
@@ -0,0 +1,1 @@
+var modules =[{"name":"main","file":0,"main":66,"table":{"builtin":1,"zigimg":3,"ziglyph":2,"main":0}},{"name":"builtin","file":0,"main":462,"table":{}},{"name":"ziglyph","file":0,"main":32522,"table":{}},{"name":"zigimg","file":0,"main":34283,"table":{"builtin":4}},{"name":"builtin","file":0,"main":34497,"table":{}}];
