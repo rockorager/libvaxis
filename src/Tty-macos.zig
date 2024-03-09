97c9bf1a9d60c876f410e6111cef7fa78d780a59
Rylee Lyman <rylee.alanza@gmail.com>
2024-03-09 00:23:25 -0500

    fix: use `select` on macOS
    the man page for macOS `poll(2)` says, under "bugs", that polling
    devices does not work, and indeed, although the current implementation
    based on poll(2) does produce output, it does not appear to actually
    be polling for this reason; instead the wait is blocking on the `read`
    call, causing the macOS user to need to input another character to
    exit.
    
    this change introduces a wrapper over macOS's implementation of
    `select`, modeled after `std.io.poll` as `select.zig`. it is a compile
    error to use `select.zig` when `builtin.os.tag.isDarwin()` is false. a
    lightly altered version of `Tty.zig` accompanies this change. it's
    certainly possible to incorporate the two into one file; i didn't just
    to leave the other one in its original state.
    
    with this change, writing to the pipe correctly exits.
    
    additionally, on macOS, `Thread.setName` must be called from the
    thread whose name you wish to set, so I have just commented out that
    line.

--- src/Tty-macos.zig
+++ src/Tty-macos.zig
@@ -4,20 +4,20 @@ const os = std.os;
 const Vaxis = @import("vaxis.zig").Vaxis;
 const Parser = @import("Parser.zig");
 const GraphemeCache = @import("GraphemeCache.zig");
+const select = @import("select.zig").select;
 
 const log = std.log.scoped(.tty);
 
 const Tty = @This();
 
 const Writer = std.io.Writer(os.fd_t, os.WriteError, os.write);
-
 const BufferedWriter = std.io.BufferedWriter(4096, Writer);
 
 /// the original state of the terminal, prior to calling makeRaw
 termios: os.termios,
 
-/// The file descriptor we are using for I/O
-fd: os.fd_t,
+/// the file descriptor we are using for I/O
+fd: std.fs.File,
 
 /// the write end of a pipe to signal the tty should exit its run loop
 quit_fd: ?os.fd_t = null,
@@ -27,24 +27,27 @@ buffered_writer: BufferedWriter,
 /// initializes a Tty instance by opening /dev/tty and "making it raw"
 pub fn init() !Tty {
     // Open our tty
-    const fd = try os.open("/dev/tty", .{ .ACCMODE = .RDWR }, 0);
+        const fd = try std.fs.cwd().openFile("/dev/tty", .{
+            .mode = .read_write,
+            .allow_ctty = true,
+});
 
     // Set the termios of the tty
-    const termios = try makeRaw(fd);
+    const termios = try makeRaw(fd.handle);
 
     return Tty{
         .fd = fd,
         .termios = termios,
-        .buffered_writer = std.io.bufferedWriter(Writer{ .context = fd }),
+        .buffered_writer = std.io.bufferedWriter(Writer{ .context = fd.handle }),
     };
 }
 
-/// release resources associated with the Tty return it to its original state
+/// release resources associated with the Tty and return it to its original state
 pub fn deinit(self: *Tty) void {
-    os.tcsetattr(self.fd, .FLUSH, self.termios) catch |err| {
+    os.tcsetattr(self.fd.handle, .FLUSH, self.termios) catch |err| {
         log.err("couldn't restore terminal: {}", .{err});
     };
-    os.close(self.fd);
+    self.fd.close();
 }
 
 /// stops the run loop
@@ -61,18 +64,17 @@ pub fn run(
     vx: *Vaxis(Event),
 ) !void {
     // create a pipe so we can signal to exit the run loop
-    const pipe = try os.pipe();
-    defer os.close(pipe[0]);
-    defer os.close(pipe[1]);
+    const read_end, const write_end = try os.pipe();
+    defer os.close(read_end);
+    defer os.close(write_end);
 
     // get our initial winsize
-    const winsize = try getWinsize(self.fd);
+    const winsize = try getWinsize(self.fd.handle);
     if (@hasField(Event, "winsize")) {
         vx.postEvent(.{ .winsize = winsize });
     }
 
-    // assign the write end of the pipe to our quit_fd
-    self.quit_fd = pipe[1];
+    self.quit_fd = write_end;
 
     // Build a winch handler. We need build this struct to get an anonymous
     // function which can post the winsize event
@@ -108,33 +110,39 @@ pub fn run(
             }
         }
     };
-    try WinchHandler.init(vx, self.fd);
+    try WinchHandler.init(vx, self.fd.handle);
 
     // initialize a grapheme cache
     var cache: GraphemeCache = .{};
 
-    // Set up fds for polling
-    var pollfds: [2]std.os.pollfd = .{
-        .{ .fd = self.fd, .events = std.os.POLL.IN, .revents = undefined },
-        .{ .fd = pipe[0], .events = std.os.POLL.IN, .revents = undefined },
-    };
-
     var parser: Parser = .{};
 
-    // initialize the read buffer
-    var buf: [1024]u8 = undefined;
+    // 2kb ought to be more than enough? given that we reset after each call?
+    var io_buf: [2 * 1024]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&io_buf);
+
+    // set up fds for selecting
+        var selector = try select(fba.allocator(), enum { tty, quit }, .{
+            .tty = self.fd,
+        .quit = .{ .handle = read_end },
+    });
+
     // read loop
     while (true) {
-        _ = try std.os.poll(&pollfds, -1);
-        if (pollfds[1].revents & std.os.POLL.IN != 0) {
+        fba.reset();
+        try selector.select();
+
+        if (selector.fifo(.quit).readableLength() > 0) {
             log.debug("quitting read thread", .{});
             return;
         }
 
-        const n = try os.read(self.fd, &buf);
+        const tty = selector.fifo(.tty);
+        const n = tty.readableLength();
         var start: usize = 0;
+        defer tty.discard(n);
         while (start < n) {
-            const result = try parser.parse(buf[start..n]);
+            const result = try parser.parse(tty.readableSlice(start));
             start += result.n;
             // TODO: if we get 0 byte read, copy the remaining bytes to the
             // beginning of the buffer and read mmore? this should only happen
@@ -230,6 +238,7 @@ pub fn makeRaw(fd: os.fd_t) !os.termios {
     raw.iflag.IGNCR = false;
     raw.iflag.ICRNL = false;
     raw.iflag.IXON = false;
+    raw.iflag.IUTF8 = true;
 
     raw.oflag.OPOST = false;
 
@@ -245,16 +254,12 @@ pub fn makeRaw(fd: os.fd_t) !os.termios {
     raw.cc[@intFromEnum(std.posix.V.MIN)] = 1;
     raw.cc[@intFromEnum(std.posix.V.TIME)] = 0;
     try os.tcsetattr(fd, .FLUSH, raw);
+
     return state;
 }
 
 /// The size of the terminal screen
-pub const Winsize = struct {
-    rows: usize,
-    cols: usize,
-    x_pixel: usize,
-    y_pixel: usize,
-};
+pub const Winsize = @import("Tty.zig").Winsize;
 
 fn getWinsize(fd: os.fd_t) !Winsize {
     var winsize = os.winsize{
@@ -264,8 +269,10 @@ fn getWinsize(fd: os.fd_t) !Winsize {
         .ws_ypixel = 0,
     };
 
-    const err = os.system.ioctl(fd, os.T.IOCGWINSZ, @intFromPtr(&winsize));
-    if (os.errno(err) == .SUCCESS)
+    const TIOCGWINSZ = 1074295912;
+    const err = os.system.ioctl(fd, @as(c_int, TIOCGWINSZ), @intFromPtr(&winsize));
+    const e = os.errno(err);
+    if (e == .SUCCESS)
         return Winsize{
             .rows = winsize.ws_row,
             .cols = winsize.ws_col,
@@ -273,4 +280,34 @@ fn getWinsize(fd: os.fd_t) !Winsize {
             .y_pixel = winsize.ws_ypixel,
         };
     return error.IoctlError;
+}
+
+test "run" {
+    if (true) return error.SkipZigTest;
+    const TestEvent = union(enum) {
+        winsize: Winsize,
+        key_press: @import("Key.zig"),
+    };
+
+    var vx = try Vaxis(TestEvent).init(.{});
+    defer vx.deinit(null);
+    var tty = try init();
+    defer tty.deinit();
+
+    const inner = struct {
+        fn f(t: *Tty) void {
+            std.time.sleep(std.time.ns_per_s);
+            t.stop();
+        }
+    };
+
+    const pid = try std.Thread.spawn(.{}, inner.f, .{&tty});
+    defer pid.join();
+
+    try tty.run(TestEvent, &vx);
+}
+
+test "get winsize" {
+    const tty = try init();
+    _ = try getWinsize(tty.fd);
 }
