c5891399f118669303b15f130f3784aad35bd803
Tim Culverhouse <tim@timculverhouse.com>
2024-03-11 09:38:52 -0500

    api: export Screen and gwidth
    Signed-off-by: Tim Culverhouse <tim@timculverhouse.com>

--- src/main.zig
+++ src/main.zig
@@ -12,11 +12,13 @@ pub const Style = Cell.Style;
 pub const Color = Cell.Color;
 pub const Image = @import("Image.zig");
 pub const Mouse = @import("Mouse.zig");
+pub const Screen = @import("Screen.zig");
 pub const Winsize = @import("Tty.zig").Winsize;
 pub const Window = @import("Window.zig");
 
 pub const ziglyph = @import("ziglyph");
 pub const widgets = @import("widgets.zig");
+pub const gwidth = @import("gwidth.zig");
 
 /// Initialize a Vaxis application.
 pub fn init(comptime Event: type, opts: Options) !Vaxis(Event) {
