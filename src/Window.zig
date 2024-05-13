rockorager/libvaxis

commit f7f4606f1d49430473fac376f7e10a5c03fac2c3

window(fill): bounds check the memcpy op
Bounds check our window before performing any direct buffer access. This
affects clear and fill, and allows a window to technically be out of
bounds, but only the visual portion of the window will be filled.

src/Window.zig
@@ -221,9 +221,10 @@ pub fn fill(self: Window, cell: Cell) void {
     } else {
         // Non-contiguous. Iterate over rows an memset
         var row: usize = self.y_off;
-        while (row < (self.height + self.y_off)) : (row += 1) {
+        const last_row = @min(self.height + self.y_off, self.screen.height - self.y_off);
+        while (row < last_row) : (row += 1) {
             const start = self.x_off + (row * self.screen.width);
-            const end = start + self.width;
+            const end = @min(start + self.width, start + (self.screen.width - self.x_off));
             @memset(self.screen.buf[start..end], cell);
         }
     }

commit ca4346e35a6b9dbf1b9f679b46a7e591ce21749d

window(fill): fix bounds check
The bounds check should terminate at the window height.

Fixes: f7f4606f1d49 "window(fill): bounds check the memcpy op"

src/Window.zig
@@ -221,7 +221,7 @@ pub fn fill(self: Window, cell: Cell) void {
     } else {
         // Non-contiguous. Iterate over rows an memset
         var row: usize = self.y_off;
-        const last_row = @min(self.height + self.y_off, self.screen.height - self.y_off);
+        const last_row = @min(self.height + self.y_off, self.screen.height);
         while (row < last_row) : (row += 1) {
             const start = self.x_off + (row * self.screen.width);
             const end = @min(start + self.width, start + (self.screen.width - self.x_off));