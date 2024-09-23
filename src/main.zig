22ca57f246f733fbb22dc7ee2e97e3bc4be72963
00JCIV00 <jake.crawfordiv@gmail.com>
2024-09-22 20:12:26 -0400

    view: implemented an easy mechanism for vertical and horizontal scrolling
    - Implemented View as an easy way to use the existing Screen and Window APIs for rendering Cells that don't fit within a Window. Basically, a user renders the oversized Cell content to a View, then renders a part of that View to a Window.
    - Created the `view.zig` example.

--- src/main.zig
+++ src/main.zig
@@ -30,6 +30,7 @@ pub const GraphemeCache = @import("GraphemeCache.zig");
 pub const grapheme = @import("grapheme");
 pub const Event = @import("event.zig").Event;
 pub const Unicode = @import("Unicode.zig");
+pub const View = @import("View.zig");
 
 /// The target TTY implementation
 pub const Tty = switch (builtin.os.tag) {
