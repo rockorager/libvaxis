889b57d7e33cab9e66cdddef32e78a01527ec1b4
Tim Culverhouse <tim@timculverhouse.com>
2024-10-14 19:14:51 -0500

    widgets(alignment): replace initChild call

--- src/widgets/alignment.zig
+++ src/widgets/alignment.zig
@@ -3,5 +3,10 @@ const Window = @import("../Window.zig");
 pub fn center(parent: Window, cols: usize, rows: usize) Window {
     const y_off = (parent.height / 2) -| (rows / 2);
     const x_off = (parent.width / 2) -| (cols / 2);
-    return parent.initChild(x_off, y_off, .{ .limit = cols }, .{ .limit = rows });
+    return parent.child(.{
+        .x_off = x_off,
+        .y_off = y_off,
+        .width = .{ .limit = cols },
+        .height = .{ .limit = rows },
+    });
 }
