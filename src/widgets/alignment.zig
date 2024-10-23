65df41bf3a0dd79dd9904917404504df2aa342ab
Justin Braben <jbraben@dmsiworks.com>
2024-10-23 08:50:51 -0600

    Add new alignment widgets

--- src/widgets/alignment.zig
+++ src/widgets/alignment.zig
@@ -10,3 +10,47 @@ pub fn center(parent: Window, cols: u16, rows: u16) Window {
         .height = rows,
     });
 }
+
+pub fn topLeft(parent: Window, cols: u16, rows: u16) Window {
+    const y_off: u16 = 0;
+    const x_off: u16 = 0;
+    return parent.child(.{
+        .x_off = x_off,
+        .y_off = y_off,
+        .width = .{ .limit = cols },
+        .height = .{ .limit = rows }
+    });
+}
+
+pub fn topRight(parent: Window, cols: u16, rows: u16) Window {
+    const y_off: u16 = 0;
+    const x_off = parent.width -| cols;
+    return parent.child(.{
+        .x_off = x_off,
+        .y_off = y_off,
+        .width = .{ .limit = cols },
+        .height = .{ .limit = rows }
+    });
+}
+
+pub fn bottomLeft(parent: Window, cols: u16, rows: u16) Window {
+    const y_off = parent.height -| rows;
+    const x_off: u16 = 0;
+    return parent.child(.{
+        .x_off = x_off,
+        .y_off = y_off,
+        .width = .{ .limit = cols },
+        .height = .{ .limit = rows }
+    });
+}
+
+pub fn bottomRight(parent: Window, cols: u16, rows: u16) Window {
+    const y_off = parent.height -| rows;
+    const x_off = parent.width -| cols;
+    return parent.child(.{
+        .x_off = x_off,
+        .y_off = y_off,
+        .width = .{ .limit = cols },
+        .height = .{ .limit = rows }
+    });
+}
