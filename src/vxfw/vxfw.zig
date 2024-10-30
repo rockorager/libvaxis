38eba29d0a160bfa1f28414f2eb61c703db975dd
Tim Culverhouse <tim@timculverhouse.com>
2024-10-30 15:16:04 -0500

    vxfw: add SizedBox widget

--- src/vxfw/vxfw.zig
+++ src/vxfw/vxfw.zig
@@ -18,6 +18,7 @@ pub const FlexRow = @import("FlexRow.zig");
 pub const ListView = @import("ListView.zig");
 pub const Padding = @import("Padding.zig");
 pub const RichText = @import("RichText.zig");
+pub const SizedBox = @import("SizedBox.zig");
 pub const Text = @import("Text.zig");
 pub const TextField = @import("TextField.zig");
 
