c65ac6b69311db8901560ed39e9e7034cee0b461
Tim Culverhouse <tim@timculverhouse.com>
2024-10-30 06:31:52 -0500

    zig fmt

--- src/widgets/alignment.zig
+++ src/widgets/alignment.zig
@@ -14,43 +14,23 @@ pub fn center(parent: Window, cols: u16, rows: u16) Window {
 pub fn topLeft(parent: Window, cols: u16, rows: u16) Window {
     const y_off: u16 = 0;
     const x_off: u16 = 0;
-    return parent.child(.{
-        .x_off = x_off,
-        .y_off = y_off,
-        .width = cols,
-        .height = rows
-    });
+    return parent.child(.{ .x_off = x_off, .y_off = y_off, .width = cols, .height = rows });
 }
 
 pub fn topRight(parent: Window, cols: u16, rows: u16) Window {
     const y_off: u16 = 0;
     const x_off = parent.width -| cols;
-    return parent.child(.{
-        .x_off = x_off,
-        .y_off = y_off,
-        .width = cols,
-        .height = rows
-    });
+    return parent.child(.{ .x_off = x_off, .y_off = y_off, .width = cols, .height = rows });
 }
 
 pub fn bottomLeft(parent: Window, cols: u16, rows: u16) Window {
     const y_off = parent.height -| rows;
     const x_off: u16 = 0;
-    return parent.child(.{
-        .x_off = x_off,
-        .y_off = y_off,
-        .width = cols,
-        .height = rows
-    });
+    return parent.child(.{ .x_off = x_off, .y_off = y_off, .width = cols, .height = rows });
 }
 
 pub fn bottomRight(parent: Window, cols: u16, rows: u16) Window {
     const y_off = parent.height -| rows;
     const x_off = parent.width -| cols;
-    return parent.child(.{
-        .x_off = x_off,
-        .y_off = y_off,
-        .width = cols,
-        .height = rows
-    });
+    return parent.child(.{ .x_off = x_off, .y_off = y_off, .width = cols, .height = rows });
 }
