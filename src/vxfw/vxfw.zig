c88d777cd3ebb6882b337a51f5d5718269c3be2b
Tim Culverhouse <tim@timculverhouse.com>
2024-12-31 07:48:32 -0600

    vxfw: add Border widget

--- src/vxfw/vxfw.zig
+++ src/vxfw/vxfw.zig
@@ -11,6 +11,7 @@ const Allocator = std.mem.Allocator;
 pub const App = @import("App.zig");
 
 // Widgets
+pub const Border = @import("Border.zig");
 pub const Button = @import("Button.zig");
 pub const Center = @import("Center.zig");
 pub const FlexColumn = @import("FlexColumn.zig");
