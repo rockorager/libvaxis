00e222ce1127fb64aa9badbdb145fb003788cba0
Tim Culverhouse <tim@timculverhouse.com>
2025-02-08 08:25:27 -0600

    root: fix default panic handler

--- src/main.zig
+++ src/main.zig
@@ -59,9 +59,9 @@ pub const Panic = struct {
 };
 
 /// Resets terminal state on a panic, then calls the default zig panic handler
-pub fn panic_handler(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
+pub fn panic_handler(msg: []const u8, _: ?*std.builtin.StackTrace, ret_addr: ?usize) noreturn {
     recover();
-    std.debug.defaultPanic(msg, error_return_trace, ret_addr);
+    std.debug.defaultPanic(msg, ret_addr);
 }
 
 /// Resets the terminal state using the global tty instance. Use this only to recover during a panic
