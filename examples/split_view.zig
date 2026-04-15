91a5cfcf0065555e37944088a4f4792a8c49d3ae
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-04-14 21:00:14 -0500

    get split_view example working

--- examples/split_view.zig
+++ examples/split_view.zig
@@ -49,13 +49,12 @@ const Model = struct {
     }
 };
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+    const allocator = init.gpa;
 
-    const allocator = gpa.allocator();
-
-    var app = try vxfw.App.init(allocator);
+    var app: vxfw.App = undefined;
+    try app.init(io, allocator, init.environ_map);
     defer app.deinit();
 
     const model = try allocator.create(Model);
