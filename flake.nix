c1e1f23be38951c425cdf31af455ba23ef178940
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-04-20 10:17:30 -0500

    use Zig 0.16 from nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,24 +2,20 @@
   description = "libvaxis";
   inputs = {
     nixpkgs = {
-      url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
-    };
-    zig = {
-      url = "git+https://git.ocjtech.us/jeff/zig-overlay.git";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      url = "https://channels.nixos.org/nixos-unstable-small/nixexprs.tar.xz";
     };
   };
   outputs =
     {
       nixpkgs,
-      zig,
       ...
     }:
     let
-      lib = nixpkgs.lib;
-      platforms = lib.attrNames zig.packages;
+      platforms = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
       packages =
         system:
         import nixpkgs {
@@ -35,7 +31,7 @@
             pkgs.fd
             pkgs.neovim
             pkgs.pinact
-            zig.packages.${pkgs.stdenv.hostPlatform.system}."0.16.0"
+            pkgs.zig_0_16
           ];
         };
       });
